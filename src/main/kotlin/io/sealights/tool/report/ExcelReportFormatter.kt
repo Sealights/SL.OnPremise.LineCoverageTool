f3bce2c0ebfbbb65c228b35c96608bb804acf26a
Gregory Futa <gregory.futa@sealights.io>
2024-05-09 15:36:08 +0200

    [SLDEV-17781] Git Diff parsing

--- src/main/kotlin/io/sealights/tool/report/ExcelReportFormatter.kt
+++ src/main/kotlin/io/sealights/tool/report/ExcelReportFormatter.kt
@@ -2,6 +2,7 @@ package io.sealights.tool.report
 
 import arrow.core.Either
 import io.sealights.tool.FileName
+import io.sealights.tool.LineWithCoverage
 import io.sealights.tool.MethodLinesWithCoverage
 import org.apache.poi.ss.usermodel.CellStyle
 import org.apache.poi.ss.usermodel.Row
@@ -20,21 +21,109 @@ class ExcelReportFormatter(
 ) {
     private lateinit var workbook: Workbook
     private lateinit var sheet: Sheet
+    private var lastWrittenRowIndex: Int = 1
+    private lateinit var lineNumberCellStyle: CellStyle
+    private lateinit var lineNumberModifiedCellStyle: CellStyle
+    private lateinit var sourceCellStyle: CellStyle
+    private lateinit var sourceModifiedCellStyle: CellStyle
+    private lateinit var coveredCellStyle: CellStyle
+    private lateinit var uncoveredCellStyle: CellStyle
 
     fun createReport(dataToProcess: Map<FileName, Set<MethodLinesWithCoverage>>): Either<Error, String> {
         createExcelWorkbook()
+        createCellStyles()
         createHeader()
         createAppBranchBuildNameRow()
+        createSourceCodeRows(dataToProcess)
 
         saveWorkbook()
 
         return Either.Right("SUCCESS")
     }
 
+    private fun createCellStyles() {
+        lineNumberCellStyle = FormattingStyles.createLineNumberCellStyle(workbook, false)
+        lineNumberModifiedCellStyle = FormattingStyles.createLineNumberCellStyle(workbook, true)
+        sourceCellStyle = FormattingStyles.createSourceLineCellStyle(workbook, false)
+        sourceModifiedCellStyle = FormattingStyles.createSourceLineCellStyle(workbook, true)
+        coveredCellStyle = FormattingStyles.createCoveredCellStyle(workbook, true)
+        uncoveredCellStyle = FormattingStyles.createCoveredCellStyle(workbook, false)
+
+    }
+
+    private fun createSourceCodeRows(dataToProcess: Map<FileName, Set<MethodLinesWithCoverage>>) {
+
+        dataToProcess.forEach { (filename, methodLinesWithCoverageSet) ->
+            createFilenameCell(filename)
+            createMethodsCells(methodLinesWithCoverageSet)
+            createEmptyRow()
+        }
+
+    }
+
+    private fun createMethodsCells(methodLinesWithCoverageSet: Set<MethodLinesWithCoverage>) {
+        methodLinesWithCoverageSet.forEach { methodLinesCoverage ->
+            createMethodNameCell(methodLinesCoverage.name.displayName)
+            createSourceLineCells(methodLinesCoverage.linesCoverage)
+        }
+    }
+
+    private fun createSourceLineCells(linesCoverage: List<LineWithCoverage>) {
+        linesCoverage.forEach { lineWithCoverage ->
+            run {
+                // cell with line number
+                val lineRow = sheet.createRow(fetchNewRowIndex())
+                val numberCell = lineRow.createCell(Column.B)
+                numberCell.setCellValue("${lineWithCoverage.number}")
+                numberCell.cellStyle = if (lineWithCoverage.modified) lineNumberModifiedCellStyle else lineNumberCellStyle
+
+                // source code
+                var sourceCell = lineRow.createCell(Column.E)
+                sourceCell.setCellValue(lineWithCoverage.content)
+                sourceCell.cellStyle = if (lineWithCoverage.modified) sourceModifiedCellStyle else sourceCellStyle
+
+                // empty cells before source code
+                sourceCell = lineRow.createCell(Column.C)
+                sourceCell.cellStyle = if (lineWithCoverage.modified) sourceModifiedCellStyle else sourceCellStyle
+                sourceCell = lineRow.createCell(Column.D)
+                sourceCell.cellStyle = if (lineWithCoverage.modified) sourceModifiedCellStyle else sourceCellStyle
+
+                // changed code column
+                val changedCodeCell = lineRow.createCell(Column.F)
+                changedCodeCell.setCellValue(
+                    if (lineWithCoverage.modified) "Yes" else "-"
+                )
+                changedCodeCell.cellStyle = if (lineWithCoverage.modified) sourceModifiedCellStyle else sourceCellStyle
+                
+                // changed code column
+                val coveredCodeCell = lineRow.createCell(Column.G)
+                coveredCodeCell.setCellValue(
+                    if (lineWithCoverage.covered) "Yes" else "No"
+                )
+                coveredCodeCell.cellStyle = if (lineWithCoverage.covered) coveredCellStyle else uncoveredCellStyle
+            }
+        }
+    }
+
+    private fun createMethodNameCell(methodDisplayName: String) {
+        val filenameCellStyle = FormattingStyles.createFilenameStyle(workbook)
+        val filenameRow = sheet.createRow(fetchNewRowIndex())
+        createHeaderCell(filenameRow, 3, methodDisplayName, filenameCellStyle)
+    }
+
+    private fun createFilenameCell(filename: FileName) {
+        val filenameCellStyle = FormattingStyles.createFilenameStyle(workbook)
+        val filenameRow = sheet.createRow(fetchNewRowIndex())
+        createHeaderCell(filenameRow, 2, filename, filenameCellStyle)
+    }
+
+    private fun createEmptyRow() = fetchNewRowIndex()
+    private fun fetchNewRowIndex(): Int = ++lastWrittenRowIndex
+
     private fun createAppBranchBuildNameRow() {
         val headerStyle = FormattingStyles.createAppBranchBuildStyle(workbook)
         val row = sheet.createRow(1)
-        createHeaderCell(row, 0, appBranchBuild, headerStyle)
+        createHeaderCell(row, Column.A, appBranchBuild, headerStyle)
     }
 
     private fun saveWorkbook() {
@@ -51,13 +140,13 @@ class ExcelReportFormatter(
         val headerStyle = FormattingStyles.createHeaderStyle(workbook)
 
         val headerRow = sheet.createRow(0)
-        createHeaderCell(headerRow, 0, "App / Branch / Build", headerStyle)
-        createHeaderCell(headerRow, 1, "Line", headerStyle)
-        createHeaderCell(headerRow, 2, "File", headerStyle)
-        createHeaderCell(headerRow, 3, "Method", headerStyle)
-        createHeaderCell(headerRow, 4, "Code", headerStyle)
-        createHeaderCell(headerRow, 5, "Changed Code", headerStyle)
-        createHeaderCell(headerRow, 6, "Covered Code", headerStyle)
+        createHeaderCell(headerRow, Column.A, "App / Branch / Build", headerStyle)
+        createHeaderCell(headerRow, Column.B, "Line", headerStyle)
+        createHeaderCell(headerRow, Column.C, "File", headerStyle)
+        createHeaderCell(headerRow, Column.D, "Method", headerStyle)
+        createHeaderCell(headerRow, Column.E, "Code", headerStyle)
+        createHeaderCell(headerRow, Column.F, "Changed Code", headerStyle)
+        createHeaderCell(headerRow, Column.G, "Covered Code", headerStyle)
     }
 
     private fun createHeaderCell(headerRow: Row, columnIndex: Int, content: String, headerStyle: CellStyle) {
@@ -84,4 +173,14 @@ class ExcelReportFormatter(
         val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH,mm")
         return currentDateTime.format(formatter)
     }
+}
+
+object Column {
+    const val A = 0
+    const val B = 1
+    const val C = 2
+    const val D = 3
+    const val E = 4
+    const val F = 5
+    const val G = 6
 }
--- src/main/kotlin/io/sealights/tool/report/FormattingStyles.kt
+++ src/main/kotlin/io/sealights/tool/report/FormattingStyles.kt
@@ -1,40 +1,108 @@
 package io.sealights.tool.report
 
 import org.apache.poi.ss.usermodel.CellStyle
+import org.apache.poi.ss.usermodel.Color
 import org.apache.poi.ss.usermodel.FillPatternType
 import org.apache.poi.ss.usermodel.IndexedColors
 import org.apache.poi.ss.usermodel.Workbook
+import org.apache.poi.xssf.usermodel.XSSFCellStyle
+import org.apache.poi.xssf.usermodel.XSSFColor
+import org.apache.poi.xssf.usermodel.XSSFFont
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 
 
 object FormattingStyles {
 
-    fun createHeaderStyle(workbook: Workbook): CellStyle {
-        val headerStyle: CellStyle = workbook.createCellStyle()
-        headerStyle.fillForegroundColor = IndexedColors.GREY_25_PERCENT.index
-        headerStyle.fillPattern = FillPatternType.SOLID_FOREGROUND
+    private val WHITE_COLOR = createXSSFColor(255, 255, 255)
+    private val LIGHT_GRAY_COLOR = createXSSFColor(217, 217, 217)
+    private val LIGHT_RED_COLOR = createXSSFColor(244, 204, 204)
+    private val LIGHTER_GRAY_COLOR = createXSSFColor(239, 239, 239)
+    private val LIGHT_GREEN_COLOR = createXSSFColor(217, 234, 211)
+    private val LIGHT_BLUE_COLOR = createXSSFColor(207, 226, 243)
+    private val FONT_GRAY_COLOR = createXSSFColor(153, 153, 153)
+    private val FONT_BLACK_COLOR = createXSSFColor(0, 0, 0)
+    private const val FONT_NAME_DEFAULT = "Calibri"
 
-        val font = (workbook as XSSFWorkbook).createFont()
-        font.fontName = "Calibri"
-        font.fontHeightInPoints = 11.toShort()
-        font.bold = true
+    fun createHeaderStyle(workbook: Workbook): CellStyle {
+        val headerStyle = createCellStyle(workbook as XSSFWorkbook, fillColor = LIGHT_GRAY_COLOR)
+        val font = createFont(workbook, bold = true)
         headerStyle.setFont(font)
-        
+
+        return headerStyle
+    }
+
+    fun createFilenameStyle(workbook: Workbook): CellStyle {
+        val headerStyle = createCellStyle(workbook as XSSFWorkbook)
+        val font = createFont(workbook, bold = true)
+        headerStyle.setFont(font)
         return headerStyle
     }
 
     fun createAppBranchBuildStyle(workbook: Workbook): CellStyle {
-        val cellStyle = workbook.createCellStyle()
-//        headerStyle.fillForegroundColor = IndexedColors.GREY_25_PERCENT.index
-//        headerStyle.fillPattern = FillPatternType.SOLID_FOREGROUND
+        val cellStyle = createCellStyle(workbook as XSSFWorkbook)
+        val font = createFont(workbook)
+        cellStyle.setFont(font)
+        return cellStyle
+    }
 
-        val font = (workbook as XSSFWorkbook).createFont()
-        font.fontName = "Calibri"
-        font.fontHeightInPoints = 11.toShort()
-        font.bold = false
+    fun createLineNumberCellStyle(workbook: Workbook, modified: Boolean): CellStyle {
+        val cellStyle = createCellStyle(workbook as XSSFWorkbook, fillColor = returnOnCondition(modified, LIGHT_BLUE_COLOR, LIGHTER_GRAY_COLOR))
+        val font = createFont(workbook, color = FONT_GRAY_COLOR)
         cellStyle.setFont(font)
 
         return cellStyle
     }
+
+    fun createSourceLineCellStyle(workbook: Workbook, modified: Boolean): CellStyle {
+        val cellStyle = createCellStyle(workbook as XSSFWorkbook, fillColor = returnOnCondition(modified, LIGHT_BLUE_COLOR, LIGHTER_GRAY_COLOR))
+        val font = createFont(workbook)
+        cellStyle.setFont(font)
+
+        return cellStyle
+    }
+
+    fun createCoveredCellStyle(workbook: Workbook, covered: Boolean): CellStyle {
+        val cellStyle = createCellStyle(workbook as XSSFWorkbook, fillColor = returnOnCondition(covered, LIGHT_GREEN_COLOR, LIGHT_RED_COLOR))
+        val font = createFont(workbook)
+        cellStyle.setFont(font)
+
+        return cellStyle
+    }
+
+    private fun createCellStyle(
+        workbook: XSSFWorkbook,
+        fillColor: Color = WHITE_COLOR,
+        fillPattern: FillPatternType = FillPatternType.SOLID_FOREGROUND
+    ): XSSFCellStyle {
+        val cellStyle = workbook.createCellStyle()
+        cellStyle.fillPattern = fillPattern
+        cellStyle.setFillForegroundColor(fillColor)
+        return cellStyle
+    }
+
+    private fun createFont(
+        workbook: XSSFWorkbook,
+        fontName: String = FONT_NAME_DEFAULT,
+        fontHeightInPoints: Short = 11.toShort(),
+        bold: Boolean = false,
+        color: XSSFColor = FONT_BLACK_COLOR
+    ): XSSFFont {
+        val font = workbook.createFont()
+        font.fontName = fontName
+        font.fontHeightInPoints = fontHeightInPoints
+        font.bold = bold
+        font.setColor(color)
+        return font
+    }
+
+    private fun <T> returnOnCondition(condition: Boolean, onTrue: T, onFalse: T) = if (condition) {
+        onTrue
+    } else {
+        onFalse
+    }
+
+
+    @OptIn(ExperimentalUnsignedTypes::class)
+    private fun createXSSFColor(red: Int, green: Int, blue: Int) = XSSFColor(ubyteArrayOf(0u, red.toUByte(), green.toUByte(), blue.toUByte()).toByteArray())
 
 }
